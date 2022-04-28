bb725ec006bb084d184facbc0fa468a1f43049aa
mdarifali <arif.raj2050@gmail.com>
2022-04-29 00:23:43 +0600

    db and express connect code

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+// Middleware //
+app.use(cors());
+app.use(express.json());
+
+// Mongo_DB Database Connections Code//
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.053o8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const databaseCollection = client.db('inventory_database').collection('productapi');
+
+        app.get('/productapi', async (req, res) => {
+            const query = {}
+            const cursor = databaseCollection.find(query);
+            const product = await cursor.toArray();
+            res.send(product);
+
+        })
+    }
+
+    finally {
+    }
+
+};
+
+run().catch(console.log('DB Connect Success'));
+
+app.get('/', (req, res) => {
+    res.send('Server is Connected');
+});
+
+app.listen(port, () => {
+    console.log(`This server is running port ${port}`);
+});
